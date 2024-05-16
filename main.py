50e8f9b82ba6213e39d6b0d8db951f3ef466b8f0
Pavol Molcsan <p.molcsan@gmail.com>
2024-05-17 00:04:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -520,4 +520,4 @@ if debug_mode:
     app.include_router(router=pgvector_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=8000), log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=8000)), log_level="info")
